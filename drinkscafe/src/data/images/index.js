be6e7a662cc2cee8b88bc875accff518406f0524
Jacob Brown <jacob.brown@shipt.com>
2023-02-16 12:04:37 -0600

    Starting drinkscafe

---
+++ drinkscafe/src/data/images/index.js
@@ -0,0 +1,15 @@
+import drink1Img from "./drink1.webp";
+import drink2Img from "./drink2.webp";
+import drink3Img from "./drink3.webp";
+import drink4Img from "./drink4.webp";
+import drink5Img from "./drink5.webp";
+import drink6Img from "./drink6.webp";
+
+export default [
+  drink1Img,
+  drink2Img,
+  drink3Img,
+  drink4Img,
+  drink5Img,
+  drink6Img,
+];
