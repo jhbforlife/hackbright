e3440dfdbdb85efb8c922e19b16d85e5840d64a2
Jacob Brown <jacob.brown@shipt.com>
2023-02-28 19:18:51 -0600

    Finished assignment with extras

--- tictactoe/App.js
+++ tictactoe/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import Game from './components/Game';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Game />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3A5683',
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+    ...Platform.select({
+      native: {
+        justifyContent: 'center',
+      },
+      default: {
+        justifyContent: 'flex-start',
+      }
+    })
   },
 });
